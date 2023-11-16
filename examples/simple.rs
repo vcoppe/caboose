dc42c6b8eb3cbe3a7b4d799b9d3a36a8bd8c6e75
vcoppe <vianney.coppe@gmail.com>
2023-11-16 18:27:59 +0100

    second scenario

--- examples/simple.rs
+++ examples/simple.rs
@@ -23,7 +23,7 @@ struct Model {
 }
 
 fn main() {
-    if false {
+    if true {
         nannou::app(model).update(update).run();
     } else {
         let mut model = get_model();
@@ -56,16 +56,46 @@ fn get_model() -> Model {
     let graph = simple_graph(graph_size);
     let transition_system = Arc::new(SimpleWorld::new(graph.clone()));
 
+    let to_id = |x: usize, y: usize| GraphNodeId(x + y * graph_size);
+
     let mut tasks = vec![];
-    for (from, to) in vec![(0, 9), (9, 0), (1, 8), (8, 1), (2, 7), (7, 2), (3, 6)] {
+    for (from, to) in vec![
+        /*((4, 8), (8, 1)),
+        ((5, 9), (3, 6)),
+        ((9, 5), (0, 7)),
+        ((2, 8), (7, 9)),
+        ((4, 3), (8, 8)),
+        ((2, 5), (0, 2)),
+        ((8, 2), (1, 4)),
+        ((8, 9), (1, 6)),
+        ((2, 7), (9, 4)),
+        ((1, 0), (7, 5)),
+        ((0, 9), (3, 8)),
+        ((9, 1), (7, 3)),
+        ((5, 1), (0, 5)),*/
+        ((0, 0), (0, 9)),
+        ((0, 9), (0, 0)),
+        ((0, 1), (0, 8)),
+        ((0, 8), (0, 1)),
+        ((0, 2), (0, 7)),
+        ((0, 7), (0, 2)),
+        ((0, 3), (0, 6)),
+    ]
+    .iter()
+    .map(|((x1, y1), (x2, y2))| (to_id(*x1, *y1), to_id(*x2, *y2)))
+    {
         tasks.push(Arc::new(Task::new(
-            SimpleState(GraphNodeId(from)),
-            SimpleState(GraphNodeId(to)),
+            SimpleState(from),
+            SimpleState(to),
             OrderedFloat(0.0),
         )));
     }
 
-    let colors = vec![BLUE, GREEN, RED, GOLD, HOTPINK, ORANGE, PURPLE, TEAL];
+    // A set of 20 colors that are visually distinct
+    let colors = vec![
+        BLUE, GREEN, RED, GOLD, HOTPINK, ORANGE, PURPLE, TEAL, YELLOW, CYAN, PINK, LIME, MAROON,
+        NAVY, OLIVE, LAVENDER, BROWN, BEIGE, CORAL, GREY, MAGENTA, TURQUOISE,
+    ];
 
     let pivots = Arc::new(tasks.iter().map(|t| t.goal_state.clone()).collect());
     let heuristic_to_pivots = Arc::new(
